c6b2bc5c45b670c8e6a3d1bdbd1c219b59c35838
Joshua Chinchilla <JoshuaChinchilla91@gmail.com>
2020-08-01 10:24:11 -0700

    endpoints for MVP are completed

--- server.js
+++ server.js
@@ -27,9 +27,63 @@ server.post('/api/projects', (req, res) => {
             res.status(201).json(newProjectEntry);
         })
     })
-    .catch(err => {
-        console.log('POST error', err);
+    .catch(error => {
+        console.log('POST error', error);
         res.status(500).json({ message: 'Failed to store project' });
+    })
+})
+
+
+// GET Resources
+server.get('/api/resources', (req, res) => {
+    db('resources')
+    .then(resources => {
+        res.status(200).json(resources);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+});
+
+
+// POST Resources, Adding a resource
+server.post('/api/resources', (req, res) => {
+    const resourceData = req.body;
+    db('resources').insert(resourceData)
+    .then(ids => {
+        db('resources').where({ id: ids[0] })
+        .then(newResourceEntry => {
+            res.status(201).json(newResourceEntry);
+        })
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'Failed to store resource', error });
+    });
+});
+
+// GET Tasks
+server.get('/api/tasks', (req, res) => {
+    db('tasks')
+    .then(tasks => {
+        res.status(200).json(tasks)
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+});
+
+// POST Tasks, Adding a task
+server.post('/api/tasks', (req, res) => {
+    const taskData = req.body;
+    db('tasks').insert(taskData)
+    .then(ids => {
+        db('tasks').where({ id: ids[0] })
+        .then(newTaskEntry => {
+            res.status(201).json(newTaskEntry);
+        })
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'Failed to store task', error });
     })
 })
 
